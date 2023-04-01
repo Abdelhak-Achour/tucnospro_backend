670c81508b72cfe743b8d823dad77ec8cabe7e54
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-04-01 16:42:06 +0100

    working on users schema n collection

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MDBLINK);
 
 app.listen(3001, () => console.log("TucnosPro server up and running"));
