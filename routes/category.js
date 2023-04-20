323174c8779a8b526e0f5dea5514226a27622555
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-04-20 15:47:43 +0100

    added get route for categories

--- routes/category.js
+++ routes/category.js
@@ -4,6 +4,32 @@ import { verifyToken } from "../middlewares/verify.js";
 
 const router = express.Router()
 
-router.post("/", verifyToken, async (req,res) => {})
+router.get("/", async (req, res) => {
+    try
+    {
+        const categories = await CategoryModel.find();
+        res.json({categories: categories});
+    }
+    catch (err)
+    {
+        console.log(err);
+        res.json(err);
+    }
+})
+
+router.post("/", verifyToken, async (req,res) => {
+    const {name} = req.body;
+
+    const category = await CategoryModel.findOne({name});
+    if (category)
+    {
+        return res.json({message: "category already exists"});
+    }
+
+    const newCategory = new CategoryModel({name});
+    await newCategory.save();
+
+    res.json({message: "category saved"});
+})
 
 export { router as categoryRouter}
