5a530039d23b6c552490f99a09ba6054050140b5
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-04-20 20:15:55 +0100

    certain category get route

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ import { BlogModel } from "../modules/blogs.js";
 import multer from "multer";
 import path from "path";
 import { verifyToken } from "../middlewares/verify.js";
+import { CategoryModel } from "../modules/categories.js";
 
 const router = express.Router();
 
@@ -25,6 +26,28 @@ router.get("/", async (req, res) => {
     catch (err)
     {
         console.log(err);
+        res.json(err);
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    
+    try
+    {
+        var category = await CategoryModel.findById(id);
+    }
+    catch (err)
+    {
+        return res.sendStatus(404);
+    }
+
+    try{
+        const blogs = await BlogModel.find({category: category.name});
+        res.json({blogs: blogs});
+    }
+    catch (err)
+    {
         res.json(err);
     }
 })
