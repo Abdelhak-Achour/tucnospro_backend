64cabb0a55f3841f32057ad62f7e65773ee5db2a
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-05-08 18:16:14 +0100

    set up visits and views counter v1

--- routes/statistics.js
+++ routes/statistics.js
@@ -22,8 +22,29 @@ router.post("/", async (req, res) => {
     const now = new Date();
     const dateNtime = date.format(now, "MM/YYYY");
 
-    const newStat = new StatisticsModel({date: dateNtime});
-    await newStat.save();
+    const statistics = await StatisticsModel.findOne({date: dateNtime});
+
+    if(statistics)
+    {
+        const visits = statistics.visits;
+        const views = statistics.views;
+
+        if(visited)
+        {
+            statistics.visits = Number(visits) + 1;
+        }
+        if(viewed)
+        {
+            statistics.views = Number(views) + 1;
+        }
+
+        await statistics.save()
+    }
+    else
+    {
+        const newStat = new StatisticsModel({visits: 1, views: 1, date: dateNtime});
+        await newStat.save();
+    }
 
     res.json({message: "stats saved"});
 })
